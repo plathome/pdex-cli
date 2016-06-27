f0b7acd97a28b3fe2cb49956300d2383d4028782
elias hasnat <elias@plathome.co.jp>
2016-06-27 19:25:14 +0900

    updated the rest-client library

--- cli.go
+++ cli.go
@@ -4,10 +4,25 @@ import (
 	"fmt"
 	"os"
 	"github.com/urfave/cli"
-	"gopkg.in/resty.v0"
+//	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/url"
 //	"github.com/plathome/pdex-cli/conf"
 	"./conf"
 )
+
+// type HmacApiResponse struct {
+// 	HmacValue string `json:"digest"`
+// }
+
+type Result struct {
+    Digest string
+}
+
+// type HMACS struct {
+//     Hmac  string
+// }
 
 func main() {
 	app := cli.NewApp()
@@ -32,7 +47,7 @@ func main() {
 						configuration.CreateConfig()
 						confs := &configuration.ConfigFile{
 							PdexUrl: c.Args().First(),
-							AccessKey: "ACCESS-KEY",
+							AccessKey: c.Args().Get(1),
 						}
 						configuration.WriteConfigs(confs)
 						fmt.Fprint(os.Stdout, "Success: register url config \n")
@@ -56,7 +71,7 @@ func main() {
 							fmt.Fprint(os.Stderr, "Error: Failed reading config file. \n")
 							os.Exit(1)
 						}
-						CheckPing(conf.PdexUrl)
+						GetUtils(conf.PdexUrl,"/api/v1/utils/ping")
 						return nil
 					},
 				},
@@ -70,7 +85,7 @@ func main() {
 							fmt.Fprint(os.Stderr, "Error: Failed reading config file. \n")
 							os.Exit(1)
 						}
-						CheckVersion(conf.PdexUrl)
+						GetUtils(conf.PdexUrl,"/api/v1/utils/version")
 						return nil
 					},
 				},
@@ -84,7 +99,21 @@ func main() {
 							fmt.Fprint(os.Stderr, "Error: Failed reading config file. \n")
 							os.Exit(1)
 						}
-						CheckChangeLog(conf.PdexUrl)
+						GetUtils(conf.PdexUrl,"/api/v1/utils/changelog")
+						return nil
+					},
+				},
+				{
+					Name:    "hmac",
+					Aliases: []string{"c"},
+					Usage:   "util hmac",
+					Action: func(c *cli.Context) error {
+						conf, err := configuration.ReadConfigs()
+						if err != nil {
+							fmt.Fprint(os.Stderr, "Error: Failed reading config file. \n")
+							os.Exit(1)
+						}
+						DeviceSendMessage(conf.PdexUrl + "/api/v1/utils/hmac", "e63ab20b0771", "01.8529e6.26387394")
 						return nil
 					},
 				},
@@ -100,18 +129,12 @@ func main() {
 					Aliases: []string{"s"},
 					Usage:   "device send",
 					Action: func(c *cli.Context) error {
-						if c.Args().First() == "" {
-							fmt.Fprint(os.Stderr, "Error: Please entry the url. \n")
-							os.Exit(1)
-						}
 						conf, err := configuration.ReadConfigs()
 						if err != nil {
 							fmt.Fprint(os.Stderr, "Error: Failed reading config file. \n")
 							os.Exit(1)
 						}
-						fmt.Println(conf.PdexUrl)
-						fmt.Println(conf.AccessKey)
-						//DeviceSendMessage(c.Args().First())
+						DeviceSendMessage(conf.PdexUrl + "/api/v1/utils/hmac", "e63ab20b0771", "01.8529e6.26387394")
 						return nil
 					},
 				},
@@ -122,53 +145,48 @@ func main() {
 	app.Run(os.Args)
 }
 
-func CheckPing(url string) {
-	resp, err := resty.R().Get(url + "/api/v1/utils/ping")
-	if err != nil {
-		fmt.Printf("\nError: %v", err)
-	}
-	fmt.Printf("%v\n", resp)
-}
-
-func CheckVersion(url string) {
-	resp, err := resty.R().Get(url + "/api/v1/utils/version")
-	if err != nil {
-		fmt.Printf("\nError: %v", err)
-	}
-	fmt.Printf("%v\n", resp)
-}
-
-func CheckChangeLog(url string) {
-	resp, err := resty.R().Get(url + "/api/v1/utils/changelog")
-	if err != nil {
-		fmt.Printf("\nError: %v", err)
-	}
-	fmt.Printf("%v\n", resp)
-}
-
-func DeviceSendMessage(conf string) {
-	// resp, err := resty.R().Get("http://localhost:9292/api/v1/utils/changelog")
-	// if err != nil {
-	// 	fmt.Printf("\nError: %v", err)
-	// }
-	// fmt.Printf("%v\n", resp)
-	fmt.Printf("\n%v\n",conf)
+func DeviceSendMessage(urlstr string, secretkey string, deid string) {
+	PostRequest(urlstr, []string{"key","message"} , []string{secretkey,deid} )
 }
 
 func ReadMessageByApp() {
-	// resp, err := resty.R().Get("http://localhost:9292/api/v1/utils/changelog")
-	// if err != nil {
-	// 	fmt.Printf("\nError: %v", err)
-	// }
-	// fmt.Printf("%v\n", resp)
 	fmt.Printf("%v\n","MessageByAppInfo")
 }
 
 func ReadMessageByChannel() {
-	// resp, err := resty.R().Get("http://localhost:9292/api/v1/utils/changelog")
-	// if err != nil {
-	// 	fmt.Printf("\nError: %v", err)
-	// }
-	// fmt.Printf("%v\n", resp)
 	fmt.Printf("%v\n","ReadMessageByChannel")
 }
+
+func PostRequest(link string, parameters []string, values []string) (body string, err error) {
+   data := url.Values{}
+   for i := range parameters {
+      data.Set(parameters[i], values[i])
+   }
+   resp, err := http.PostForm(link, data)
+   if err != nil {
+      return "", err
+   }
+   defer resp.Body.Close()
+   ret, err := ioutil.ReadAll(resp.Body)
+   if err != nil {
+      return "", err
+   }
+   fmt.Println(string(ret))
+   return string(ret), nil
+}
+
+func GetUtils(urlstr string, utils string) {
+	resp, err := http.Get(urlstr + utils)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+	htmlData, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(string(htmlData))
+}
+
